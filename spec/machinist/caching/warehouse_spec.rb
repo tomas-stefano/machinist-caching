811ce7a58896855c674645d74507fb278bffdb87
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-07-14 19:13:37 -0300

    Prefer composition over inheritance on Machinist::Caching::Warehouse

--- spec/machinist/caching/warehouse_spec.rb
+++ spec/machinist/caching/warehouse_spec.rb
@@ -1,22 +1,16 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require 'spec_helper'
 
-describe Machinist::Warehouse do
-  it "should store and retrieve values" do
-    warehouse = Machinist::Warehouse.new
-    warehouse[1, 2] = "example"
-    warehouse[1, 2].should == "example"
+describe Machinist::Caching::Warehouse do
+  let :warehouse do
+    Machinist::Caching::Warehouse.new
   end
 
-  it "should return an empty array for a new key" do
-    warehouse = Machinist::Warehouse.new
-    warehouse[1, 2].should == []
-    warehouse[3, 4] << "example"
-    warehouse[3, 4].should == ["example"]
+  it 'should return nil for a new key' do
+    warehouse['klass', 'attributes'].should == nil
   end
 
-  it "should clone" do
-    warehouse = Machinist::Warehouse.new
-    warehouse[1, 2] = "example"
-    warehouse.clone.should == warehouse
+  it 'should store and retrieve values' do
+    warehouse['klass', 'attributes'] = 'object'
+    warehouse['klass', 'attributes'].should == 'object'
   end
 end
